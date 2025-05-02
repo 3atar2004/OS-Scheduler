e76ae30064bf7479da3359bc9440fe7e49437bf3
Omar Reda <omar.reda13231@gmail.com>
2025-05-02 16:04:07 +0300

    Cleaner main menu UI

--- process_generator.c
+++ process_generator.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
         printf("Invalid number of arguments!\n");
         exit(1);
     }
-     // 1. Read the input files.
+    // 1. Read the input files.
     CircularQueue *PCBs = (CircularQueue *)malloc(sizeof(CircularQueue));
     initQueue(PCBs);
     FILE *pFile;
@@ -23,31 +23,31 @@ int main(int argc, char *argv[])
         exit(1);
     }
     int id, arrival, runtime, priority;
-    fscanf(pFile,"%*[^\n]\n");
-    printf("-----------------------------Processes------------------------------\n");
+    fscanf(pFile, "%*[^\n]\n");
+    printf("----------------------------------Processes--------------------------------------\n");
+    printf("\t\tID\t   Arrival Time\t   Running Time\t    Priority\n");
     int count = 1;
-    while(fscanf(pFile,"%d\t%d\t%d\t%d\n",&id,&arrival,&runtime,&priority)==4)
+    while (fscanf(pFile, "%d\t%d\t%d\t%d\n", &id, &arrival, &runtime, &priority) == 4)
     {
         printf("Process %d:\t", count);
-        printf("%d\t%d\t%d\t%d\n",id,arrival,runtime,priority);
+        printf("%d\t\t%d\t\t%d\t\t%d\n", id, arrival, runtime, priority);
         PCB *readpcb = malloc(sizeof(PCB));
-        printf("Testing ID: %d", id);
         readpcb->id = id;
         readpcb->arrival_time = arrival;
         readpcb->runtime = runtime;
         readpcb->priority = priority;
-        enqueue(PCBs,readpcb); 
+        enqueue(PCBs, readpcb);
         count++;
     }
     fclose(pFile);
-    //printQueue(PCBs);
+    // printQueue(PCBs);
 
     // 2. Read the chosen scheduling algorithm and its parameters, if there are any from the argument list.
     ///    1- Non-preemptive Highest Priority First (HPF)
     ///    2- Shortest Remaining Time Next (SRTN)
     ///    3- Round Robin (RR)
     int chosenAlgorithm;
-    printf("Enter a scheduling algorithm:\n");
+    printf("\nEnter a scheduling algorithm:\n");
     printf("1 - Non-preemptive Highest Priority First (HPF)\n");
     printf("2 - Shortest Remaining Time Next (SRTN)\n");
     printf("3 - Round Robin (RR)\n");
@@ -88,24 +88,24 @@ int main(int argc, char *argv[])
     case 2:
         printf("Shortest Remaining Time Next (SRTN)\n");
         break;
-    case 3:   
-        printf("Round Robin (RR) with quantum %d\n",quantum);
+    case 3:
+        printf("Round Robin (RR) with quantum %d\n", quantum);
         break;
     default:
         break;
     }
 
     // 3. Initiate and create the scheduler and clock processes.
-    int schedulerid=fork();
-    if (schedulerid == -1) 
+    int schedulerid = fork();
+    if (schedulerid == -1)
     {
         perror("Error forking scheduler!");
         exit(1);
     }
-    if(schedulerid==0)
+    if (schedulerid == 0)
     {
-        int compilestatus=system("gcc scheduler.c -o scheduler.out");
-        if(compilestatus==0)
+        int compilestatus = system("gcc scheduler.c -o scheduler.out");
+        if (compilestatus == 0)
         {
             printf("Scheduler process started!\n");
             execl("./scheduler.out", "scheduler.out", chosenAlgorithm_str, quantum_str, NULL); // runing the scheduler with chosen algorithm and quantum
@@ -118,18 +118,18 @@ int main(int argc, char *argv[])
             exit(1);
         }
     }
-  
+
     // 4. Use this function after creating the clock process to initialize clock.
-    int clckid=fork();
-    if (clckid == -1) 
+    int clckid = fork();
+    if (clckid == -1)
     {
         perror("Error forking clock");
         exit(1);
     }
-    if(clckid==0)
+    if (clckid == 0)
     {
-        int clkcompilestatus=system("gcc clk.c -o clk.out");
-        if(clkcompilestatus==0)
+        int clkcompilestatus = system("gcc clk.c -o clk.out");
+        if (clkcompilestatus == 0)
         {
             printf("Clock process started!\n");
             execl("./clk.out", "clk.out", NULL); // runing the clock
@@ -143,7 +143,7 @@ int main(int argc, char *argv[])
         }
     }
     initClk();
-    // To get time use this function. 
+    // To get time use this function.
     int x = getClk();
     printf("Current Time is %d\n", x);
     // TODO Generation Main Loop
@@ -151,33 +151,34 @@ int main(int argc, char *argv[])
     // 6. Send the information to the scheduler at the appropriate time.
     key_t msg_id;
     int send_val;
-    msg_id=ftok("msgqueue", 65);
-    int msgq_id=msgget(msg_id, IPC_CREAT | 0666);
-    if(msgq_id==-1)
+    msg_id = ftok("msgqueue", 65);
+    int msgq_id = msgget(msg_id, IPC_CREAT | 0666);
+    if (msgq_id == -1)
     {
         printf("Error in creating message queue\n");
         exit(-1);
     }
     struct msgbuff process;
-     while(!isEmpty(PCBs))
+    while (!isEmpty(PCBs))
     {
-        PCB* currentpcb;
-        dequeue(PCBs,&currentpcb);
-        process.pcb.id=currentpcb->id;
-        process.pcb.pid=-1; // -1 means hasn't been forked before
-        process.pcb.arrival_time=currentpcb->arrival_time;
-        process.pcb.start_time=-1; // means hasn't started before
-        process.pcb.runtime=currentpcb->runtime;
-        process.pcb.remaining_time=currentpcb->runtime;
-        process.pcb.waiting_time=0;
-        process.pcb.priority=currentpcb->priority;
-        process.pcb.state=READY;
-        while(getClk() < currentpcb->arrival_time);
-        printf("Sending process %d with arrival time %d and runtime %d and priority %d\n",process.pcb.id,process.pcb.arrival_time,process.pcb.runtime,process.pcb.priority);
+        PCB *currentpcb;
+        dequeue(PCBs, &currentpcb);
+        process.pcb.id = currentpcb->id;
+        process.pcb.pid = -1; // -1 means hasn't been forked before
+        process.pcb.arrival_time = currentpcb->arrival_time;
+        process.pcb.start_time = -1; // means hasn't started before
+        process.pcb.runtime = currentpcb->runtime;
+        process.pcb.remaining_time = currentpcb->runtime;
+        process.pcb.waiting_time = 0;
+        process.pcb.priority = currentpcb->priority;
+        process.pcb.state = READY;
+        while (getClk() < currentpcb->arrival_time)
+            ;
+        printf("Sending process %d with arrival time %d and runtime %d and priority %d\n", process.pcb.id, process.pcb.arrival_time, process.pcb.runtime, process.pcb.priority);
         printf("Current Clock Time: %d\n", getClk());
-        process.mtype=1;
-        send_val=msgsnd(msgq_id,&process,sizeof(process.pcb),!IPC_NOWAIT);
-        if(send_val==-1)
+        process.mtype = 1;
+        send_val = msgsnd(msgq_id, &process, sizeof(process.pcb), !IPC_NOWAIT);
+        if (send_val == -1)
         {
             printf("Error in sending message\n");
         }
@@ -186,7 +187,6 @@ int main(int argc, char *argv[])
     kill(schedulerid, SIGUSR2); // send signal that i won't be sending any proccesses.
     int stat;
     wait(&stat); // wait for scheduler to finish
-
 
     // 7. Clear clock resources
     msgctl(msgq_id, IPC_RMID, (struct msqid_ds *)0);
